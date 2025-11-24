f5812e702bc5c48cdabe09f533e5f80c4d08b2f4
Raphael Rezende <53958298+Raphael-Rezende@users.noreply.github.com>
2025-11-24 19:02:04 -0300

    Initialize item generator during database startup

--- Redux/Database/ServerDatabase.cs
+++ Redux/Database/ServerDatabase.cs
@@ -11,6 +11,9 @@ namespace Redux.Database
             Context = new ConquerDataContext();
             NHibernateHelper.BuildSessionFactory();
 
+            // Initialize the item generator seed before any new items are created
+            Context.Items.PopulateItemGenerator();
+
             Context.Accounts.ResetLoginTokens();
             Context.Characters.ResetOnlineCharacters();
             return true;
